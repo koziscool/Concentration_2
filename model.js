1d5401c044d01b38ed061b92ae9633a066f4627f
koziscool <kozjob@yahoo.com>
2017-08-05 15:51:36 -0500

    again

--- model.js
+++ model.js
@@ -7,6 +7,8 @@ var matcherModel = {
     matchedCards: 0,
     totalCards: 0,
 
+    currentId: 1,
+
     gameStateText: "You haven't won yet, keep going.",
 
     cardValues: [ "A", "B", "C", "D", "E", "F", "G", "H" ],
@@ -23,11 +25,33 @@ var matcherModel = {
     },
 
     addPair: function() {
-        this.cards.push( this.aCard );
-        this.cards.push( this.aCard );
+        var value = this.randomValue();
+        this.cards.push( new this.Card( this.getId(), value ) );
+        this.cards.push( new this.Card( this.getId(), value ) );
+        this.totalCards += 2;
     },
 
     shuffle: function() {
         console.log('shuffling');
+    },
+
+
+    Card: function( id, value ){
+        this.id = id;
+        this.value = value;
+
+        this.matches = function( otherCard ){
+            return this.value === otherCard.value;
+        }
+    },
+
+    randomValue: function() {
+        return this.cardValues[ Math.floor(Math.random() * this.cardValues.length) ]
+    },
+
+    getId: function() {
+        var id = this.currentId;
+        this.currentId++;
+        return id;
     }
 }
